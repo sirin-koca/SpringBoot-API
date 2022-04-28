9cadaed0d7b1f25d2b2de6b65d63c87b8c071751
Sirin Koca <s182211@oslomet.no>
2022-04-28 17:58:20 +0200

    application.properties updated

--- src/main/java/oslomet/web/motorvogn/MotorvognController.java
+++ src/main/java/oslomet/web/motorvogn/MotorvognController.java
@@ -1,4 +1,17 @@
 package oslomet.web.motorvogn;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MotorvognController {
+
+    @Autowired
+    private MotorvognRepository rep;
+
+    @PostMapping ("/regMotorvogn")
+    public void regMotorvogn(Motorvogn innMotorvogn){
+        rep.regMotorvogn(innMotorvogn);
+    }
 }
--- src/main/java/oslomet/web/motorvogn/MotorvognRepository.java
+++ src/main/java/oslomet/web/motorvogn/MotorvognRepository.java
@@ -1,4 +1,41 @@
 package oslomet.web.motorvogn;
 
-public interface MotorvognRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class MotorvognRepository {
+
+    @Autowired
+    private JdbcTemplate db;
+
+    public void regMotorvogn(Motorvogn innMotorvogn) {
+        String sql =
+                "INSERT INTO Motorvogn (personnr, navn, adresse, kjennetegn, merke, type) VALUES(?,?,?,?,?,?)";
+        db.update(sql,
+                innMotorvogn.getPersonnr(),
+                innMotorvogn.getNavn(),
+                innMotorvogn.getAdresse(),
+                innMotorvogn.getKjennetegn(),
+                innMotorvogn.getMerke(),
+                innMotorvogn.getType()
+        );
+    }
+
+    public List<Motorvogn> hentAlleMotorvogn() {
+        String sql = "SELECT * FROM Motorvogn";
+        List<Motorvogn> alleBiletter = db.query(sql,
+                new BeanPropertyRowMapper(Motorvogn.class));
+        return alleBiletter;
+    }
+
+    public void slettAlleMotorvogn() {
+        String sql = "DELETE FROM Motorvogn";
+        db.update(sql);
+
+    }
 }
